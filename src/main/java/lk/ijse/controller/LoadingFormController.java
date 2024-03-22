7556506b918a2ec621550418ef1b2a9313062e0f
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-03-22 22:06:26 +0530

    delete signIn ,signUp controllers and fxml file

--- src/main/java/lk/ijse/controller/LoadingFormController.java
+++ src/main/java/lk/ijse/controller/LoadingFormController.java
@@ -1,11 +1,20 @@
 package lk.ijse.controller;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Duration;
 import lk.ijse.bo.LoadingTask;
+
+import java.io.IOException;
 
 public class LoadingFormController {
     @FXML
@@ -19,20 +28,29 @@ public class LoadingFormController {
 
     LoadingTask loadingTask = new LoadingTask();
 
-    public void initialize(){
-        loadingTask.progressProperty().addListener((ob,oldValue,newValue)->{
-            String formatnumber = String.format("%.0f", newValue.doubleValue()*100);
-            loading.setText(formatnumber +"%");
-            subProgressBar.setWidth(mainProgressBar.getWidth()*newValue.doubleValue());
+    public void initialize() {
+        loadingTask.progressProperty().addListener((ob, oldValue, newValue) -> {
+            String formatnumber = String.format("%.0f", newValue.doubleValue() * 100);
+            loading.setText(formatnumber + "%");
+            subProgressBar.setWidth(mainProgressBar.getWidth() * newValue.doubleValue());
 
-           if (newValue.doubleValue()==1.0){
+            if (newValue.doubleValue() == 1.0) {
                Window window = loading.getScene().getWindow();
-               Stage stage = (Stage) window;
-               stage.close();
-           }
+                Stage stage =  (Stage) window ;
+                stage.close();
+
+                try {
+                    Stage stage1 = new Stage();
+                    stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
+                    stage1.show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
 
         });
         new Thread(loadingTask).start();
-    }
 
+    }
 }
--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,67 @@
 package lk.ijse.controller;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 public class LoginFormController {
+
+    @FXML
+    private Pane parent;
+
+    @FXML
+    private VBox vBox;
+
+    TranslateTransition t = new TranslateTransition(Duration.seconds(1),vBox);
+    public void initialize(){
+        t.setToX(vBox.getLayoutX() * 20);
+        t.play();
+
+        t.setOnFinished((e)->{
+            try {
+                vBox.getChildren().removeAll();
+                vBox.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/view/signUp_form.fxml")));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+        t.setToX(vBox.getLayoutX() * 20);
+        t.play();
+
+        t.setOnFinished((e)->{
+            try {
+                vBox.getChildren().removeAll();
+                vBox.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/view/signIn_form.fxml")));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    @FXML
+    void btnSignOnAction(ActionEvent event) {
+        t.setToX(0);
+        t.play();
+
+        t.setOnFinished((e)->{
+            try {
+                 FXMLLoader.load(getClass().getResource("/view/signUp_form.fxml.fxml"))
+                vBox.getChildren().removeAll();
+                vBox.getChildren().setAll();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
 }
